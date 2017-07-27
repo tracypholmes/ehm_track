a07a16a62487ac2943b8b471f47ee565ccb01f79
tracypholmes <tracypholmes@gmail.com>
2017-07-27 00:54:13 -0500

    added validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  validates :name, :username, presence: true, uniqueness: true 
+  has_many :user_health  
+  has_many :health, through: :user_health  
+  has_many :medications, through: :health  
+  has_secure_password
+  
+  validates :name, :username, :email, presence: true, uniqueness: true 
   validates :password, presence: true, length: { in: 8..15 }
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  # validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
