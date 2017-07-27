619882ccfc1bf89cd55f27319108dbbd9a34c0a7
tracypholmes <tracypholmes@gmail.com>
2017-07-27 00:19:30 -0500

    updated with starter User rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name, username, email, and password' do
+    user = User.new(
+      name: 'Pat Richols',
+      username: 'prichols',
+      email: 'prichols@prichols.me',
+      password_digest: 'patsword',
+    )
+    expect(user).to be_valid
+  end
+  
+  it 'is invalid without a name' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+  
+  it 'is invalid without a username' do
+    user = User.new(username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank!")
+  end
+  
+  it 'is invalid without an email address' do
+    user = User.new(email: "jjj$me.com")
+    expect(user).to be_valid
+  end
+  
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      name: 'Pat Richols',
+      username: 'prichols',
+      email: 'prichols@prichols.me',
+      password_digest: 'patsword',
+    )
+    user = User.new(
+      name: 'Paul Richols',
+      username: 'prichols',
+      email: 'prichols@prichols.me',
+      password_digest: 'patsword',
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("oops - this has already been taken")
+  end
+  
+    
+
+  # it "returns a user's full name as a string"
 end
