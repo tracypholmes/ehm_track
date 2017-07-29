56b72811c0fc3b995865e2becc72abed4c0fb4db
tracypholmes <tracypholmes@gmail.com>
2017-07-28 22:53:46 -0500

    updated User email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   
   validates :name, :username, :email, presence: true, uniqueness: true 
   validates :password, presence: true, length: { in: 8..15 }
-  # validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
