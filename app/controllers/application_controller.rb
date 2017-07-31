a095fe125f1320446d140b6484f676df353b2cb8
tracypholmes <tracypholmes@gmail.com>
2017-07-30 21:21:20 -0500

    Updated App controller with helper methods notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # The Helper methods needed to help us in our controllers and our views
+  #   is_logged_in?
+  #   current_user
+  #   authenticate_user! (if a user isn't logged in, send them to a login screen)
 end
