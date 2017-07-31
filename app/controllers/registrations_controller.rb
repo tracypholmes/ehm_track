a446e1c6066b195361078aa7f1ed98c55c853b2f
tracypholmes <tracypholmes@gmail.com>
2017-07-30 21:17:59 -0500

    Generated Registrations controller plus notes

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,6 @@
+class RegistrationsController < ApplicationController
+  # A form to create a user account with name, email, username, and password
+  #   User signup
+  #   User information updates (edit)
+  #   User deletion
+end
