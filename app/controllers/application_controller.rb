da6b4d814eb51c56da9448fa419a90664b5bf03a
tracypholmes <tracypholmes@gmail.com>
2017-07-31 22:49:17 -0500

    def  logged_in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_methods :current_user, :is_logged_in?, :authenticate_user!
-  
+  helper_methods :current_user, :logged_in?, :authenticate_user!
+
   # The Helper methods needed to help us in our controllers and our views
   #   is_logged_in?
   #   current_user
   #   authenticate_user! (if a user isn't logged in, send them to a login screen)
-  
   def index; end
-  
+
   private
-  
+
   def current_user # check if a current_user has been defined, if not look for a user using the user_id key in the session
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  
-  
+
+  def logged_in?
+    !!current_user
+  end
+
+
 end
