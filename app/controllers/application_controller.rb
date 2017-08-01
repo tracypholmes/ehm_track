d3952a71f54b0232b356fc78f0282ae4b1fef2b0
tracypholmes <tracypholmes@gmail.com>
2017-07-31 23:04:26 -0500

    cleaned up formatting & comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,13 @@ class ApplicationController < ActionController::Base
   # The Helper methods needed to help us in our controllers and our views
   #   is_logged_in?
   #   current_user
-  #   authenticate_user! (if a user isn't logged in, send them to a login screen)
+  #   authenticate_user! (if user isn't logged in, send them to a login screen)
   def index; end
 
   private
 
-  def current_user # check if a current_user has been defined, if not look for a user using the user_id key in the session
+  def current_user # check if a current_user has been defined
+    # if not look for a user using the user_id key in the session
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
