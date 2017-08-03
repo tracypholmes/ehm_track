49eb17667f3f816dadd05c1ae2ff569e18b56573
tracypholmes <tracypholmes@gmail.com>
2017-08-03 02:29:55 -0500

    Updated RegistrationsContr with user_params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,17 +3,25 @@ class RegistrationsController < ApplicationController
   #   User signup
   #   User information updates (edit)
   #   User deletion
-  
+
   def new
     @user = User.new
     # load a form for a User object
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      # per SG, we use sessions becuz cookies live in the browser
+      redirect_to health_path
+    else
+      render :new
     end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
   end
 end
