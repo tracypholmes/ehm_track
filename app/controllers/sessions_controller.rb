91a0a16f87f519e77b886372b59ff7bcb7573b6a
tracypholmes <tracypholmes@gmail.com>
2017-08-03 02:29:25 -0500

    updated sessions controller with new & create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,18 @@ class SessionsController < ApplicationController
   # A form to login a user with email and password
   #   User login
   #   User logout
-  def new
-
+def new
+    @user = User.new
+    # load a form for a User object
   end
-
+  
   def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to health_path
+    else
+      render :new
+    end
   end
-
-  def destroy
-
-  end
-
-
 end
