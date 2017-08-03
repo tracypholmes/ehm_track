44badada005d089d5a809bac2522ae2453cf4051
tracypholmes <tracypholmes@gmail.com>
2017-08-03 01:03:59 -0500

    started building out methods for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
   # A form to login a user with email and password
   #   User login
   #   User logout
+  def new
+
+  end
+
+  def create
+  end
+
+  def destroy
+
+  end
+
+
 end
