fbcb4e4450ee440c5fd08af8a3d5210e8ddfc422
tracypholmes <tracypholmes@gmail.com>
2017-08-03 01:03:40 -0500

    def new and create methods - reg controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,17 @@ class RegistrationsController < ApplicationController
   #   User signup
   #   User information updates (edit)
   #   User deletion
+  
+  def new
+    @user = User.new
+    # load a form for a User object
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      # per SG, we use sessions becuz cookies live in the browser
+    end
+  end
 end
