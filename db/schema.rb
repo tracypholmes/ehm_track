29fd1b6fa11ae3bc233a267ce634ba10dca83eae
tracypholmes <tracypholmes@gmail.com>
2017-08-03 01:01:22 -0500

    health migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727042256) do
+ActiveRecord::Schema.define(version: 20170803042228) do
+
+  create_table "healths", force: :cascade do |t|
+    t.string "issue_name"
+    t.date "date_started"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
