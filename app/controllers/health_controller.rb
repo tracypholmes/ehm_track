bf7adcddb52415a2b8783365e25125724f38ca55
tracypholmes <tracypholmes@gmail.com>
2017-08-03 02:40:04 -0500

    health controller defined methods - not complete

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,16 +1,30 @@
 class HealthController < ApplicationController
   before_action :authenticate_user!
-  
+
+  def index
+    @healths = Health.all
+  end
+
   def new
+    @health = Health.new
   end
-  
+
   def create
+    @health = Health.new(health_params)
+    if @health.save
+      redirect_to @health
+    else
+      render :new
+    end
   end
-  
-  def update
+
+  def show
+    @health = Health.find_by(id: params[:id])
   end
-  
-  def destroy
+
+  private
+
+  def health_params
+    params.require(:health).permit(:issue_name, :date_started)
   end
-  
 end
