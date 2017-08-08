a4125d270f775a28d49754a353ea19bb0783a4ed
tracypholmes <tracypholmes@gmail.com>
2017-08-08 08:44:22 -0500

    formatting cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :user_issue  
-  has_many :issue, through: :user_issue  
-  has_many :medications, through: :issue  
+  has_many :user_issue
+  has_many :issue, through: :user_issue
+  has_many :medications, through: :issue
   has_secure_password
-  
-  validates :name, :username, :email, presence: true, uniqueness: true 
+
+  validates :name, :username, :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 8..15 }
 end
