c0be97b204df6a272f77e9f35775f9173cf5a944
tracypholmes <tracypholmes@gmail.com>
2017-08-07 19:28:26 -0500

    updated route to correct misnamed resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :issues
   resources :medications
-  # root path
-  root to: 'application#index'
-  
-  resources :issue
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  # root path
+  root to: 'application#index'
 end
