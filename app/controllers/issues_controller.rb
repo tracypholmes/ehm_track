77ba98530de54af29828b3cb132392f06a13de42
tracypholmes <tracypholmes@gmail.com>
2017-08-08 08:43:02 -0500

    added edit/update/destroy to issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,14 +1,18 @@
 class IssuesController < ApplicationController
-  before_action :authenticate_user!
-  
+  # before_action :authenticate_user!
+
   def index
     @issues = Issue.all
   end
-  
+
+  def show
+    @issue = Issue.find_by(id: params[:id])
+  end
+
   def new
     @issue = Issue.new
   end
-  
+
   def create
     @issue = Issue.new(issue_params)
     if @issue.save
@@ -17,15 +21,29 @@ class IssuesController < ApplicationController
       render :new
     end
   end
-  
-  def show
-    @issue = Issue.find_by(id: params[:id])
+
+  def edit
+    @issue = Issue.find(params[:id])
   end
-  
+
+  def update
+    @issue = Issue.find(params[:id])
+    if @issue.update(issue_params)
+      redirect_to @issue
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @issue = Issue.find(params[:id])
+    @issue.destroy
+    redirect_to issues_path
+  end
+
   private
-  
+
   def issue_params
     params.require(:issue).permit(:issue_name, :date_started)
   end
 end
-
