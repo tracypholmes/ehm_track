c0a245d11cf28942ca48bd4f1487d9dd8edc7ee6
tracypholmes <tracypholmes@gmail.com>
2017-08-07 20:08:53 -0500

    Added code back into IssuesController

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,31 @@
 class IssuesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @issues = Issue.all
+  end
+  
+  def new
+    @issue = Issue.new
+  end
+  
+  def create
+    @issue = Issue.new(issue_params)
+    if @issue.save
+      redirect_to @issue
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @issue = Issue.find_by(id: params[:id])
+  end
+  
+  private
+  
+  def issue_params
+    params.require(:issue).permit(:issue_name, :date_started)
+  end
 end
+
