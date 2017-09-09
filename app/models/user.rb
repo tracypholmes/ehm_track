a09ebd63661da356a3e3a553d4ff1c6bbd437987
tracypholmes <tracypholmes@gmail.com>
2017-09-09 00:21:54 -0500

    got rid of automatic user creation, updated with  username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,19 +9,42 @@ class User < ApplicationRecord
   has_many :issue, through: :user_issue
   has_many :medications, through: :issue
 
-  # validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-        user = User.create(
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
+    # unless user
+    #     user = User.create(
+    #        email: data['email'],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    # user
+  end
+  
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_h).first
+  #   end
+  # end
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
-    user
   end
 end
