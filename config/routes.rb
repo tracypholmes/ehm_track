b5be654c90459f3880695cafe39582d92d9d7d77
tracy p holmes <tracypholmes@gmail.com>
2017-09-20 01:48:25 -0500

    Moved up root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
-  resources :issues
   root 'issues#index'
+  resources :issues
   resources :medications
   # resources :registrations, only: [:new, :create]
   # resources :sessions, only: [:new, :create, :destroy]
