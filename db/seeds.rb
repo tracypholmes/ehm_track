80a07d66f58e2672499701b472f37ade228a1645
tracy p holmes <tracypholmes@gmail.com>
2017-09-25 23:08:27 -0500

    Added list of symptom seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+symptom_list = [
+  [ "Abdominal Pain" ],
+  [ "Anemia"],
+  [ "Back Pain" ],
+  [ "Blood in Stool" ],
+  [ "Blood in Urine" ],
+  [ "Chest Pain" ],
+  [ "Conjunctivitis" ],
+  [ "Constipation" ],
+  [ "Cough" ],
+  [ "Dark Urine" ],
+  [ "Diarrhea" ],
+  [ "Dizziness" ],
+  [ "Erectile dysfunction" ],
+  [ "Fatigue" ],
+  [ "Fever" ],
+  [ "Foot pain" ],
+  [ "Frequent Urination" ],
+  [ "Gas" ],
+  [ "Headache" ],
+  [ "Heel Pain" ],
+  [ "Hives" ],
+  [ "Insomnia" ],
+  [ "Itching" ],
+  [ "Joint Pain" ],
+  [ "Knee Pain" ],
+  [ "Loss of Appetite" ],
+  [ "Low Grade Fever" ],
+  [ "Morning Sickness" ],
+  [ "Nausea" ],
+  [ "Night Sweats" ],
+  [ "Rash" ],
+  [ "Sleep Paralysis" ],
+  [ "Sneezing" ],
+  [ "Sore Throat" ],
+  [ "Stuffy Nose" ],
+  [ "Swollen Feet" ],
+  [ "Vaginal Discharge" ],
+  [ "Vaginal Itching" ],
+  [ "Vertigo" ]
+]
+
+symptom_list.each do |name|
+  Symptom.create( symptom_name: name )
+end
