935fb5faaa4a4bb7240ab63cd98593934ba671f4
tracy p holmes <tracypholmes@gmail.com>
2017-09-26 00:41:37 -0500

    removed unneeded comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
   helper_method :user_signed_in?, :current_user, :user_session
 
-  # The Helper methods needed to help us in our controllers and our views
-  #   is_logged_in?
-  #   current_user
-  #   authenticate_user! (if user isn't logged in, send them to a login screen)
   def index
     
   end
 
-  # private
-
-  # def current_user # check if a current_user has been defined
-  #   # if not look for a user using the user_id key in the session
-  #   @current_user ||= User.find_by(id: session[:user_id])
-  # end
-
-  # def user_signed_in?
-  #   !!current_user
-  # end
-
-  # def authenticate_user!
-  #   redirect_to new_view_session_path unless user_signed_in?
-  # end
-  
   protected
 
   def configure_permitted_parameters
