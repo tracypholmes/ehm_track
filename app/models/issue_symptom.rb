65e71797f4bece479d1211c4fe693d9859248ae4
tracy p holmes <tracypholmes@gmail.com>
2017-09-25 22:05:26 -0500

    removed validations & made assoc optional

--- app/models/issue_symptom.rb
+++ app/models/issue_symptom.rb
@@ -1,8 +1,8 @@
 class IssueSymptom < ApplicationRecord
-  belongs_to :issue
-  belongs_to :symptom
+  belongs_to :issue, optional: true
+  belongs_to :symptom, optional: true
   
-  validates :issue_id, presence: true
-  validates :symptom_id, presence: true
-  validates :symptom_id, uniqueness: { scope: :issue_id }
+  # validates :issue_id, presence: true
+  # validates :symptom_id, presence: true
+  # validates :symptom_id, uniqueness: { scope: :issue_id }
 end
