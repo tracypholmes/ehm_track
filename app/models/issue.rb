0d801be7d697d00d1b7d3d6c57bf7e6c56ade131
tracy p holmes <tracypholmes@gmail.com>
2017-09-25 20:01:28 -0500

    updated assoc with is_sym join

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,8 @@
 class Issue < ApplicationRecord
-  # has_many :users, through: :user_issue
   belongs_to :user
   has_many :medications
+  has_many :issue_symptoms
+  has_many :symptoms, through: :issue_symptoms
   
   validates :issue_name, presence: true
   validates :date_started, presence: true
