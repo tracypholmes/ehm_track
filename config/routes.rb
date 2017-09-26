6cce576280d46616f2843c28cd97d5fbfba309fb
tracy p holmes <tracypholmes@gmail.com>
2017-09-25 20:00:56 -0500

    symptoms views, spec & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'symptoms/new'
+
+  get 'symptoms/edit'
+
   devise_for :views
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   root 'issues#index'
   resources :issues
   resources :medications
+  resources :symptoms
   # resources :registrations, only: [:new, :create]
   # resources :sessions, only: [:new, :create, :destroy]
   # root path
