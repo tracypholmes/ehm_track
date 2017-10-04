8fa96c9e48b9bb4922e25e8a23f7d4509040d414
tracy p holmes <tracypholmes@gmail.com>
2017-10-04 12:27:00 -0500

    Updated relationships - still think something's not right

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,11 +1,11 @@
 class Issue < ApplicationRecord
   belongs_to :user
-  has_many :medications
+
   has_many :issue_medications
   has_many :medications, through: :issue_medications
   has_many :issue_symptoms
   has_many :symptoms, through: :issue_symptoms
-  accepts_nested_attributes_for :medications
+  accepts_nested_attributes_for :issue_medications
   
   validates :issue_name, presence: true
   validates :date_started, presence: true
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,5 +1,4 @@
 class Medication < ApplicationRecord
   has_many :issue_medications
   has_many :issues, through: :issue_medications
-  belongs_to :issue
 end
