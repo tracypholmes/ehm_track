c5668c54d7a85ae4ff92e1b7eb543d75ee0bb9d9
tracy p holmes <tracypholmes@gmail.com>
2017-10-04 13:22:40 -0500

    removed accepts_nested_attributes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,7 +5,7 @@ class Issue < ApplicationRecord
   has_many :medications, through: :issue_medications
   has_many :issue_symptoms
   has_many :symptoms, through: :issue_symptoms
-  accepts_nested_attributes_for :issue_medications
+  # accepts_nested_attributes_for :medications
   
   validates :issue_name, presence: true
   validates :date_started, presence: true
--- app/models/issue_medication.rb
+++ app/models/issue_medication.rb
@@ -1,5 +1,4 @@
 class IssueMedication < ApplicationRecord
   belongs_to :issue, optional: true
   belongs_to :medication, optional: true
-  accepts_nested_attributes_for :medication
 end
