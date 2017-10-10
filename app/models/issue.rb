8469507337a14c50d5c45ecabf6e66044681bbb8
tracy p holmes <tracypholmes@gmail.com>
2017-10-09 22:19:03 -0500

    updated with med attributes method to prevent duplication

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,8 +5,15 @@ class Issue < ApplicationRecord
   has_many :medications, through: :issue_medications
   has_many :issue_symptoms
   has_many :symptoms, through: :issue_symptoms
-  accepts_nested_attributes_for :issue_medications
+  accepts_nested_attributes_for :medications
   
   validates :issue_name, presence: true
   validates :date_started, presence: true
+  
+  def medications_attributes=(medication_attributes)
+    medication_attributes.values.each do |medication_attribute|
+      medication = Medication.find_or_create_by(medication_attribute)
+      self.medications << medication
+    end
+  end
 end
