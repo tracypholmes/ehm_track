b234d98b65c23cc5347d05767c286f1abfaa71fa
tracy p holmes <tracypholmes@gmail.com>
2017-10-10 23:13:40 -0500

    Added accept_nes_attr_for :iss_meds to Medication

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,4 +1,12 @@
 class Medication < ApplicationRecord
   has_many :issue_medications
   has_many :issues, through: :issue_medications
+  
+  # added by Luke
+  accepts_nested_attributes_for :issue_medications
+  
+  def issue_medications_attributes=(issue_medications_attributes)
+  # binding.pry 
+  end
+
 end
