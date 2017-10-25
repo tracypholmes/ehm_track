a5ee25889a8ccf1552b84a3f050d325cf79d07e7
tracy p holmes <tracypholmes@gmail.com>
2017-10-24 22:59:51 -0500

    Cleaned up junky formatting

--- app/models/issue_symptom.rb
+++ app/models/issue_symptom.rb
@@ -1,8 +1,4 @@
 class IssueSymptom < ApplicationRecord
   belongs_to :issue, optional: true
   belongs_to :symptom, optional: true
-  
-  # validates :issue_id, presence: true
-  # validates :symptom_id, presence: true
-  # validates :symptom_id, uniqueness: { scope: :issue_id }
 end
