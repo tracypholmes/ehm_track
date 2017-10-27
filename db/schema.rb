95a65fc4e0e8c6a4efbfe9890bb76a2aeebec1c5
tracypholmes <tracypholmes@gmail.com>
2017-10-26 21:34:30 -0500

    generated medication resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024190431) do
+ActiveRecord::Schema.define(version: 20171027011615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20171024190431) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.text     "notes"
+  end
+
+  create_table "medications", force: :cascade do |t|
+    t.string   "medication_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "symptoms", force: :cascade do |t|
