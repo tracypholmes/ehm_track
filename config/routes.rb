20ac6707e2d095299451535873fb008339d58e62
tracy p holmes <tracypholmes@gmail.com>
2017-11-01 09:06:31 -0500

    added nested route and corresponding link

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :views
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
+  resources :users do
+    member do
+      resources :medications, only: [:index]
+    end
+  end
+  
   root 'issues#index'
   resources :issues
-  resources :medications
 end
